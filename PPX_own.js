d2eb12a8a6b560cbadc3e77107a1ddd198700521
Haobo Zhang <74122358+Altman-x@users.noreply.github.com>
2024-01-24 16:33:10 +0800

    Update PPX_own.js

--- PPX_own.js
+++ PPX_own.js
@@ -13,9 +13,8 @@ ppx
 
 *******************************
 
-^https:\/\/.+\.pipix\.com\/bds\/feed\/channel_list\/ url script-response-body https://raw.githubusercontent.com/Altman-x/Qx/main/PPX_own.js?token=GHSAT0AAAAAACNJAQKAVRPBA6Y2YGR24SNQZNQZFQQ
-^https:\/\/.+\.pipix\.com\/bds\/user\/check_in\/ url script-response-body https://raw.githubusercontent.com/Altman-x/Qx/main/PPX_own.js?token=GHSAT0AAAAAACNJAQKAVRPBA6Y2YGR24SNQZNQZFQQ
-
+^https:\/\/.+\.pipix\.com\/bds\/feed\/channel_list\/ url script-response-body https://raw.githubusercontent.com/Altman-x/Qx/main/PPX_own.js
+^https:\/\/.+\.pipix\.com\/bds\/user\/check_in\/ url script-response-body https://raw.githubusercontent.com/Altman-x/Qx/main/PPX_own.js
 hostname = *.pipix.com
 
 *******************************/
