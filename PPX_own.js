6cd05f005787feccd277d901ef7e980ad07bcb3f
Haobo Zhang <74122358+Altman-x@users.noreply.github.com>
2024-01-24 16:38:57 +0800

    Update PPX_own.js

--- PPX_own.js
+++ PPX_own.js
@@ -4,9 +4,9 @@ ppx
 
 
 *******************************
-
+# hostname = *.pipix.com
 ^https:\/\/.+\.pipix\.com\/bds\/feed\/channel_list\/ url script-response-body https://raw.githubusercontent.com/Altman-x/Qx/main/PPX_own.js
 ^https:\/\/.+\.pipix\.com\/bds\/user\/check_in\/ url script-response-body https://raw.githubusercontent.com/Altman-x/Qx/main/PPX_own.js
-hostname = *.pipix.com
+
 
 *******************************/
