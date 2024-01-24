c20918cd15a8c262e276b04f3cff3da65dd37609
Haobo Zhang <74122358+Altman-x@users.noreply.github.com>
2024-01-24 16:36:48 +0800

    Update PPX_own.js

--- PPX_own.js
+++ PPX_own.js
@@ -1,11 +1,3 @@
-// 2024-01-23 14:05
-const url = $request.url;
-const scriptEnvironment = typeof $task != 'undefined' ? 'Surge' : (typeof $loon != 'undefined' ? 'Loon' : (typeof $httpClient != 'undefined' ? 'Qx' : 'Unknown'));
-
-if (!$response.body || scriptEnvironment === 'Unknown') {
-  $done({});
-}
-
 /******************************
 
 ppx
